2dba5dfb35039bc49c45409f8b1854d6c69fb8ce
paulchin <paulchin20@yahoo.com>
2019-05-13 13:48:19 +0800

    update

---
+++ www/todo-localstorage.js
@@ -0,0 +1,11 @@
+var todoStorage = {
+  collection: []
+};
+
+todoStorage.init = function() {};
+todoStorage.hasItem = function(label) {};
+todoStorage.save = function() {};
+todoStorage.add = function(label) {};
+todoStorage.remove = function(label) {};
+todoStorage.toggleStatus = function(label) {};
+todoStorage.filter = function(status) {};
